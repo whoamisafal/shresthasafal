b93e173ed3b0de91b8e73601fa173f655751e683
safalstha142 <computerstha12@gmail.com>
2021-08-07 06:21:14 +0545

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo Website</title>
+</head>
+<body>
+    
+<h1>Welcome to the Safal Shrestha Demo Website.</h1>
+<h2>Hosting through Github and CloudFlare</h2>
+</body>
+</html>
